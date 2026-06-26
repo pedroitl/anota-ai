f9d915f012791faf0869be7d4a8bcabad3934a7a
Pedro Italo <pedroitl15@gmail.com>
2026-06-26 10:40:17 -0300

    add sidebar and layoutstaff

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,14 @@
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <h2>Sidebar</h2>
+            <ul>
+                <li>Home</li>   
+                <li>Profile</li>
+                <li>Settings</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Sidebar;
---
+++ src/layouts/StaffLayout.jsx
@@ -0,0 +1,10 @@
+function StaffLayout() {
+  return (
+    <div className="staff-layout">
+      <aside className="sidebar"></aside>
+      <main className="content"></main>
+    </div>
+  );
+}
+
+export default StaffLayout;
